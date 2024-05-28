6719263314a44702682e8e980cc174d96c891657
Yoon-Jae-Min <jmyoon1994@naver.com>
2024-05-28 22:14:32 +0900

    20240528-mainPage-update-home

--- les-chef-react/src/MainPage/mainPageFirst.jsx
+++ les-chef-react/src/MainPage/mainPageFirst.jsx
@@ -4,7 +4,8 @@ import FirstVideo from '../../src/Video/mainFirstVideo.mp4'
 const MainFirst = () => {
     return (
         <section className='mainSection'>
-            <video muted autoPlay loop id='mainVideo' src={FirstVideo} type="video/mp4">
+            <video muted autoPlay loop id='mainVideo'>
+                <source src={FirstVideo} type="video/mp4"></source>
             </video>
         </section>
     )
