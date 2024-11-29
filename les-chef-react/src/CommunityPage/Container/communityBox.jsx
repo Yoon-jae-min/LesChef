ac69f08a20c751f35d7202a21aacfda564656271
Yoon-Jae-Min <jmyoon1994@naver.com>
2024-11-30 00:29:32 +0900

    LesChef_Development_24.11.30

--- les-chef-react/src/CommunityPage/Container/communityBox.jsx
+++ les-chef-react/src/CommunityPage/Container/communityBox.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import CommunityHeader from "./communityHeader";
 import CommunityBody from "./communityBody";
+import CommunityFooter from "./communityFooter";
+import CommunityWriteBox from "./communityWriteBox";
 
 const CommunityBox = () => {
+    const [ writeBoxVisible, setWriteBoxVisible ] = useState(false);
+
+    const communityBodyTrans = () => {
+        if(writeBoxVisible){
+            setWriteBoxVisible(false);
+        }else{
+            setWriteBoxVisible(true);
+        }
+    }
+
     return(
         <div className="communityBox">
             <CommunityHeader/>
-            <CommunityBody/>
+            { !writeBoxVisible && <CommunityBody/>}
+            { !writeBoxVisible && <CommunityFooter communityBodyTrans={communityBodyTrans}/>}
+            { writeBoxVisible && <CommunityWriteBox/> }
         </div>
     )
 }
